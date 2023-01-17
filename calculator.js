3bf0392615512c1cc9ab46b5354f706784ae0bbe
Ranahossamsalah <rana.hossam.salah2017@gmail.com>
2023-01-17 20:38:01 +0300

    first commit

---
+++ calculator.js
@@ -0,0 +1,53 @@
+//divide
+function divide (x,y){
+    console.log (x / y)
+};
+divide(100,10);
+
+
+//multiply
+function multiply (x,y){
+    console.log (x * y);
+};
+multiply(100,10);
+
+
+//modulus
+function modulus (x,y){
+    console.log (x % y);
+};
+modulus(18 , 5)
+
+
+
+//negative
+function negative (x,y){
+    console.log (x - y);
+};
+negative(18 , 5)
+
+
+
+//positive
+function positive (x,y){
+    console.log (x + y);
+};
+positive(18 , 5)
+
+
+
+//squareRoot
+function squareRoot (x){
+    console.log (Math.sqrt(x));
+};
+squareRoot(24)
+
+
+
+//power
+function power (x,y){
+    console.log (Math.pow(x,y));
+};
+power(2,4)
+
+
