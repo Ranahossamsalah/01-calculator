f7b37a30379a2fd1e4b3ca4d5e755b4300ed03f2
Ranahossamsalah <rana.hossam.salah2017@gmail.com>
2023-01-26 08:59:06 +0300

    adding opertaions and ui

--- calculator.js
+++ calculator.js
@@ -1,53 +1,111 @@
-//divide
-function divide (x,y){
-    console.log (x / y)
-};
-divide(100,10);
+/** @format */
 
+var btn = Array.from(document.getElementsByClassName("btn"));
+var input = document.getElementById("input");
+var sign = ["-", "+", "x", "/", "%", "power", "root"];
+var firstNum = "";
+var secondNum = "";
+var operation = "";
+var equation = "";
+console.log(btn, input);
+console.log(btn.innerText);
+
+//divide
+function divide(x, y) {
+  return (input.innerText = x / y);
+}
 
 //multiply
-function multiply (x,y){
-    console.log (x * y);
-};
-multiply(100,10);
-
+function multiply(x, y) {
+  return (input.innerText = x * y);
+}
 
 //modulus
-function modulus (x,y){
-    console.log (x % y);
-};
-modulus(18 , 5)
-
-
+function modulus(x, y) {
+  return (input.innerText = x % y);
+}
 
 //negative
-function negative (x,y){
-    console.log (x - y);
-};
-negative(18 , 5)
-
-
+function negative(x, y) {
+  return (input.innerText = x - y);
+}
 
 //positive
-function positive (x,y){
-    console.log (x + y);
-};
-positive(18 , 5)
-
-
+function positive(x, y) {
+  return (input.innerText = x + y);
+}
 
 //squareRoot
-function squareRoot (x){
-    console.log (Math.sqrt(x));
-};
-squareRoot(24)
-
-
+function squareRoot(x) {
+  return (input.innerText = Math.sqrt(x));
+}
 
 //power
-function power (x,y){
-    console.log (Math.pow(x,y));
-};
-power(2,4)
+function power(x, y) {
+  return (input.innerText = Math.pow(x, y));
+}
 
+function addnum(num) {
 
+  num.addEventListener("click", function (e) {
+    
+    input.innerText += num.innerText;
+  //   if(num.innerText==='AC' && input.innerText){
+  //  console.log(input.innerText,num.innerText);
+  //     input.innerText = ''
+  //   }
+
+    for (i = 0; i < sign.length; i++) {
+      if (input.innerText && num.innerText == sign[i]) {
+        operation = sign[i];
+      }
+    }
+    if (num.innerText == "=") {
+      var equationTrim = input.innerText.substring(
+        0,
+        input.innerText.length - 1
+      );
+      equation = equationTrim.split(operation);
+    }
+    firstNum = equation[0];
+    secondNum = equation[1];
+
+    //negative operation
+    if (operation === "-" && firstNum && secondNum) {
+      console.log(operation, firstNum, secondNum);
+      negative(firstNum, secondNum);
+    }
+    //positive operation
+    if (operation === "+" && firstNum && secondNum) {
+      console.log(operation, firstNum, secondNum);
+      positive(firstNum, secondNum);
+    }
+    //divide operation
+    if (operation === "/" && firstNum && secondNum) {
+      console.log(operation, firstNum, secondNum);
+      divide(firstNum, secondNum);
+    }
+    //multiply operation
+    if (operation === "x" && firstNum && secondNum) {
+      console.log(operation, firstNum, secondNum);
+      multiply(firstNum, secondNum);
+    }
+    //modulus operation
+    if (operation === "%" && firstNum && secondNum) {
+      console.log(operation, firstNum, secondNum);
+      modulus(firstNum, secondNum);
+    }
+    //Power operation
+    if (operation === "power" && firstNum && secondNum) {
+      console.log(operation, firstNum, secondNum);
+      power(firstNum, secondNum);
+    }
+    //squareroot operation
+    if (operation === "root" && firstNum && secondNum) {
+      console.log(operation, firstNum, secondNum);
+      root(firstNum);
+    }
+  });
+}
+
+btn.map(addnum);
